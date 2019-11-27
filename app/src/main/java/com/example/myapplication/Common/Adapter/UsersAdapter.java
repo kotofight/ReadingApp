c67ab3777d99cd48088945657a7605797f9c0bd4
lazy-bear777 <2539144408@qq.com>
2019-11-27 21:16:45 +0800

    now

--- app/src/main/java/com/example/myapplication/Common/Adapter/UsersAdapter.java
+++ app/src/main/java/com/example/myapplication/Common/Adapter/UsersAdapter.java
@@ -1,130 +1,191 @@
 package com.example.myapplication.Common.Adapter;
-import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.Common.Bean.Users;
+import com.example.myapplication.Common.Details_01;
 import com.example.myapplication.R;
 
 import java.util.List;
 
 
 
-public class UsersAdapter extends ArrayAdapter<Users> {
-    private int recourseId;
-    clickinview clickinview;//接口
-    public UsersAdapter(Context context, int textViewRecourseId, List<Users>objects,clickinview clickinview){
-        super(context,textViewRecourseId,objects);
-        recourseId=textViewRecourseId;
-        this.clickinview=clickinview;//接口
-    }
-    //接口
-    public interface  clickinview{
-        public void click_userPicPath(View view);//点击用户头像的点击事件
-        public void click_userName(View view);//点击用户名称的点击事件
-        public void click_look(View view);//点击关注图片的点击事件
-    }
-    //（为了实现点击事件）重写下面三个方法
+public class UsersAdapter extends RecyclerView.Adapter<UsersAdapter.ViewHolder> {
 
-    @Override
-    public int getCount() {
-        return super.getCount();
-    }
+    boolean flag=true;
+    private List<Users> usersList;
+    static class ViewHolder extends RecyclerView.ViewHolder{
 
-    @Nullable
-    @Override
-    public Users getItem(int position) {
-        return super.getItem(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return super.getItemId(position);
-    }
-
-
-    @NonNull
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View userNewsView;//点击view
 
         ImageView userPicPath;//个人头像
         TextView userName;// 发布人
         TextView datetime;// 发布时间
         TextView address;//发布地址
-        ImageView look;//关注图片
+        ImageView focus;
         TextView content;// 趣事内容
         ImageView picPath;// 趣事图片路径
-        //ImageView forwardImg;// 转发图片
+        ImageView forwardImg;// 转发图片
         TextView forward;// 转发数量
-       // ImageView commentImg;// 评论图片
+        ImageView commentImg;// 评论图片
         TextView comment;// 评论数量
-        //ImageView thumbUpImg;// 点赞图片
+        ImageView thumbUpImg;// 点赞图片
         TextView thumbUp;// 点赞数量
 
+        public ViewHolder(View view){
+            super(view);
 
-        Users users=getItem(position);//获取当前项的Users实例
-        View view=LayoutInflater.from(getContext()).inflate(recourseId,parent,false);
+            userNewsView=view;//点击view
 
-        userPicPath=(ImageView) view.findViewById(R.id.usePicPath);
-        userName=(TextView)view.findViewById(R.id.userName);
-        datetime=(TextView)view.findViewById(R.id.datetime);
-        address=(TextView) view.findViewById(R.id.address);
-        look=(ImageView) view.findViewById(R.id.look);
-        content=(TextView)view.findViewById(R.id.content);
-        picPath=(ImageView) view.findViewById(R.id.picPath);
-        //forwardImg=(ImageView) view.findViewById(R.id.forwardImg);
-        forward=(TextView)view.findViewById(R.id.forward);
-        //commentImg=(ImageView) view.findViewById(R.id.commentImg);
-        comment=(TextView)view.findViewById(R.id.comment);
-        //thumbUpImg=(ImageView) view.findViewById(R.id.thumbUpImg);
-        thumbUp=(TextView)view.findViewById(R.id.thumbUp);
+            userPicPath=(ImageView) view.findViewById(R.id.userPicPath);
+            userName=(TextView)view.findViewById(R.id.userName);
+            datetime=(TextView)view.findViewById(R.id.datetime);
+            address=(TextView) view.findViewById(R.id.address);
+            focus=(ImageView) view.findViewById(R.id.focus);
+            content=(TextView)view.findViewById(R.id.content);
+            picPath=(ImageView) view.findViewById(R.id.picPath);
+            forwardImg=(ImageView) view.findViewById(R.id.forwardImg);
+            forward=(TextView)view.findViewById(R.id.forward);
+            commentImg=(ImageView) view.findViewById(R.id.commentImg);
+            comment=(TextView)view.findViewById(R.id.comment);
+            thumbUpImg=(ImageView) view.findViewById(R.id.thumbUpImg);
+            thumbUp=(TextView)view.findViewById(R.id.thumbUp);
+        }
+    }
+    public UsersAdapter(List<Users>userNewsList1){
+        usersList=userNewsList1;
+    }
 
-        userPicPath.setImageResource(users.getUserPicPath());
-        userName.setText(users.getUserName());
-        datetime.setText(users.getDatetime());
-        address.setText(users.getAddress());
-        look.setImageResource(R.drawable.look);
-        content.setText(users.getContent());
-        picPath.setImageResource(users.getPicPath());
-       // forwardImg.setImageResource(users.getForwardImg());
-        forward.setText(users.getForward());
-        //commentImg.setImageResource(users.getCommentImg());
-        comment.setText(users.getComment());
-        //thumbUpImg.setImageResource(users.getThumbUpImg());
-        thumbUp.setText(users.getThumbUp());
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
 
-        //实现用户头像、用户名称和关注图片的点击事件
-        userPicPath.setTag(position);
-        look.setTag(position);
-        userName.setTag(position);
-        //用户头像点击事件
-        userPicPath.setOnClickListener(new View.OnClickListener() {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.wh3main_item,parent,false);
+        final  ViewHolder holder=new ViewHolder(view);
+        //点击用户名跳转至详情页
+        holder.userName.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                clickinview.click_userPicPath(v);
+                int position=holder.getAdapterPosition();
+                Users users=usersList.get(position);
+
+                //点击用户名称使其跳转到该用户所对应的详情介绍界面
+//                  Intent intent=new Intent(UserNewsAdapter.this,User_DetailsActivity.class);
+//                  startActivity(intent);
+//                  Adapter是一个java类并不是一个Activity，普通的java类并不能正确获得上下文环境，
+//                  因为这个类没有在AndroidManifest文件里面注册。所以在进行条状的时候只要需要手动获取上下文环境就可以了，
+                Intent intent=new Intent(v.getContext(), Details_01.class);
+                v.getContext().startActivity(intent);
             }
         });
-        //用户名称点击事件
-        userName.setOnClickListener(new View.OnClickListener() {
+        //点击用户头像跳转至详情页
+        holder.userPicPath.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                clickinview.click_userName(v);
+                Intent intent=new Intent(v.getContext(),Details_01.class);
+                v.getContext().startActivity(intent);
             }
         });
-        //关注图片点击事件
-        look.setOnClickListener(new View.OnClickListener() {
+        //关注与取消关注
+        holder.focus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                clickinview.click_look(v);
+                int position=holder.getAdapterPosition();
+                Users userNews=usersList.get(position);
+                if(flag==true){
+                    holder.focus.setImageResource(R.drawable.focused);
+                    flag=false;
+                }
+                else{
+                    holder.focus.setImageResource(R.drawable.focus);
+                    flag=true;
+                }
             }
         });
-        return view;
+        //转发与取消转发
+        holder.forwardImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position=holder.getAdapterPosition();
+                Users userNews=usersList.get(position);
+                if(flag==true){
+                    holder.forwardImg.setImageResource(R.drawable.forward2);
+                    flag=false;
+                }
+                else{
+                    holder.forwardImg.setImageResource(R.drawable.forward);
+                    flag=true;
+                }
+            }
+        });
+        //点赞与取消点赞
+        holder.thumbUpImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position=holder.getAdapterPosition();
+                Users userNews=usersList.get(position);
+                if(flag==true){
+                    holder.thumbUpImg.setImageResource(R.drawable.thumbup2);
+                    flag=false;
+                }
+                else{
+                    holder.thumbUpImg.setImageResource(R.drawable.thumbup);
+                    flag=true;
+                }
+            }
+        });
+        //评论与取消评论
+        holder.commentImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (flag==true){
+                    holder.commentImg.setImageResource(R.drawable.comment2);
+                    flag=false;
+                }
+                else{
+                    holder.commentImg.setImageResource(R.drawable.comment);
+                    flag=true;
+                }
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Users users=usersList.get(position);
+
+        holder.userPicPath.setImageResource(users.getUserPicPath());
+        holder.userName.setText(users.getUserName());
+        holder.datetime.setText(users.getDatetime());
+        holder.address.setText(users.getAddress());
+        holder.focus.setImageResource(users.getLook());
+        holder.content.setText(users.getContent());
+        holder.picPath.setImageResource(users.getPicPath());
+        holder.forwardImg.setImageResource(users.getForwardImg());
+        holder.forward.setText(users.getForward());
+        holder.commentImg.setImageResource(users.getCommentImg());
+        holder.comment.setText(users.getComment());
+        holder.thumbUpImg.setImageResource(users.getThumbUpImg());
+        holder.thumbUp.setText(users.getThumbUp());
+
+
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return usersList.size();
+    }
+    //这里的返回值不能为0或者负数，不然无法显示布局
+    @Override
+    public int getItemCount() {
+        return usersList.size();
     }
 }
--- app/src/main/java/com/example/myapplication/Fragment/CommtieeFragment.java
+++ app/src/main/java/com/example/myapplication/Fragment/CommtieeFragment.java
@@ -8,14 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.myapplication.Common.Details_01;
-import com.example.myapplication.Common.Bean.Users;
 import com.example.myapplication.Common.Adapter.UsersAdapter;
+import com.example.myapplication.Common.AddActivity;
+import com.example.myapplication.Common.Bean.Users;
 import com.example.myapplication.R;
 
 import java.util.ArrayList;
@@ -26,7 +26,7 @@ import java.util.List;
  * Use the {@link } factory method to
  * create an instance of this fragment.
  */
-public class CommtieeFragment extends Fragment implements UsersAdapter.clickinview {
+public class CommtieeFragment extends Fragment{
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_SHOW_TEXT = "text";
 
@@ -72,79 +72,43 @@ public class CommtieeFragment extends Fragment implements UsersAdapter.clickinvi
 
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.wh2activity_main, container, false);
+
+        //点击“+”则跳转至发布动态界面
+        ImageView add1=rootView.findViewById(R.id.add1);
+        add1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), AddActivity.class);
+                startActivity(intent);
+            }
+        });
+        //点击“<-”则返回到上一个界面
         //TextView contentTv = rootView.findViewById(R.id.content_tv);
         //contentTv.setText(mContentText);
 
+//        UsersAdapter adapter=new UsersAdapter(getContext(),R.layout.wh3main_item,usersList,CommtieeFragment.this );
+//        final ListView listView=rootView.findViewById(R.id.list_view);
+//        listView.setAdapter(adapter);
 
-        initview(rootView);//初始化点击事件
         initUsers();//初始化users数据
-        UsersAdapter adapter=new UsersAdapter(getContext(),R.layout.wh3main_item,usersList,CommtieeFragment.this );
-        final ListView listView=rootView.findViewById(R.id.list_view);
-        listView.setAdapter(adapter);
-
-
+        RecyclerView recyclerView=rootView.findViewById(R.id.recycler_view);
+        //将this修改为this.getActivity()
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this.getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        UsersAdapter adapter=new UsersAdapter(usersList);
+        recyclerView.setAdapter(adapter);
         return rootView;
     }
 
-    //initview()函数
-    private  void initview(View rootView){
-        listView=rootView.findViewById(R.id.list_view);
-        userImage=rootView.findViewById(R.id.usePicPath);
-        look=rootView.findViewById(R.id.look);
-    }
-    @Override
-    public void click_userPicPath(View view) {
-        //int values=(int)view.getTag();//暂时没有用到
-       // Users users=usersList.get(values);//暂时没有用到
 
-        //点击用户头像使其跳转到该用户所对应的详情介绍界面
-        Intent intent=new Intent(getContext(), Details_01.class);
-        startActivity(intent);
-    }
-    @Override
-    public void click_userName(View view) {
-        //int values=(int)view.getTag();//暂时没有用到
-        TextView textView1=(TextView)view.findViewById(R.id.userName);
-        //点击用户名称使其跳转到该用户所对应的详情介绍界面
-        Toast.makeText(getContext(),textView1.getText(),Toast.LENGTH_SHORT).show();
 
-        //Intent intent=new Intent(getContext(),Details_01.class);
-        //startActivity(intent);
-    }
-    @Override
-    public void click_look(View view) {
-       //int values=(int)view.getTag();//暂时没有用到
-        //Users users=usersList.get(values);//暂时没有用到
-        ImageView look=(ImageView)view.findViewById(R.id.look);
-        //TextView textView1=(TextView)view.findViewById(R.id.userName);
-        //look.setTag(values);
-        if(flag==0){
-            look.setImageResource(R.drawable.looked);
-            Toast.makeText(getContext(),"你已成功关注TA！",Toast.LENGTH_SHORT).show();
-            //Toast.makeText(getContext(),textView1.getText(),Toast.LENGTH_SHORT).show();
-            flag=1;
-        }
-        else{
-            look.setImageResource(R.drawable.look);
-            Toast.makeText(getContext(),"取消关注成功！",Toast.LENGTH_SHORT).show();
-            //Toast.makeText(getContext(),textView1.getText(),Toast.LENGTH_SHORT).show();
-            flag=0;
-        }
-        listView.deferNotifyDataSetChanged();
-    }
+
+
     //初始化用户数据函数
-    private void initUsers()
-    {
-        Users user1=new Users(R.drawable.user1,"悦读APP官方","今天又是元气满满的一天呢。宜读书","2019-09-10","地址",R.drawable.background2,"200","12","100");
-        usersList.add(user1);
-        Users user2=new Users(R.drawable.user2,"书大白","什么都没有，所以要多看书啊。我滴神啊！","2019-09-01","地址",R.drawable.wh2background,"11","22","33");
-        usersList.add(user2);
-        Users user3=new Users(R.drawable.user3,"李大白","好好学习天天向上","2019-09-01","地址",R.drawable.background2,"19","22","33");
-        usersList.add(user3);
-        Users user4=new Users(R.drawable.user4,"佟湘玉","我滴神啊！我滴神啊！我滴神啊！","2019-09-01","地址",R.drawable.wh2background,"15","55","67");
-        usersList.add(user4);
-        Users user5=new Users(R.drawable.user5,"郭芙蓉","所以要多看书啊！","2019-09-01","地址",R.drawable.background2,"110","2","32");
-        usersList.add(user5);
-
+    private void initUsers(){
+        Users users1 = new Users(R.drawable.user1, "你好",R.drawable.focus,"一定要努力啊不试一试，怎么知道会不会成功呢不试一试，怎么知道会不会成功呢", "2019-10-10", "地址", R.drawable.background2, R.drawable.forward, R.string.forward, R.drawable.comment, R.string.comment, R.drawable.thumbup, R.string.thubmup);
+        usersList.add(users1);
+        Users users2 = new Users(R.drawable.user1, "你好", R.drawable.focus,"一定要努力啊不试一试，怎么知道会不会成功呢不试一试，怎么知道会不会成功呢", "2019-10-10", "地址", R.drawable.img_2, R.drawable.forward, R.string.forward, R.drawable.comment, R.string.comment, R.drawable.thumbup, R.string.thubmup);
+        usersList.add(users2);
     }
 }
